8b1bbb1fd7bee64dd3ae6a4e40e6f1c315c5b12a
Parindyaa <158250717+Parindyaa@users.noreply.github.com>
2024-03-21 22:40:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import './App.css';
+import Splash from './Splash'; // Import the Splash component
+
+function App() {
+  return (
+    <div className="App">
+      <Splash /> {/* Render the Splash component */}
+    </div>
+  );
+}
+
+export default App;
